e9d2a50707598fe3e3c1d25d44f49a3faedf499e
salehloke <58175520+salehloke@users.noreply.github.com>
2022-12-07 12:55:14 +0800

    3 files modified

--- src/app/main-page-demo/main-page-demo.component.ts
+++ src/app/main-page-demo/main-page-demo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, UntypedFormGroup } from '@angular/forms';
 import { ComponentStore } from '@ngrx/component-store';
 import { Observable } from 'rxjs';
 import { MainPageDemoStore } from './main-page-demo.store';
@@ -10,17 +11,35 @@ import { MainPageDemoStore } from './main-page-demo.store';
   providers: [MainPageDemoStore],
 })
 export class MainPageDemoComponent implements OnInit {
-  constructor(private readonly mainPageDemoStore: MainPageDemoStore) {
-    this.mainPageDemoStore.patchState({
-      pageMode: 'default',
-    });
-  }
-
   pageMode$ = this.mainPageDemoStore.pageMode$;
+  isFormReadOnly$ = this.mainPageDemoStore.isFormReadOnly$;
+
+  basicForm: UntypedFormGroup;
+  constructor(
+    private readonly mainPageDemoStore: MainPageDemoStore,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     // listen to the page mode changes
+    this.initForm();
+  }
+
+  initForm() {
+    this.basicForm = this.fb.group({
+      name: [''],
+      age: 0,
+    });
+    this.basicForm.controls.name.valueChanges.subscribe((value) => {
+      console.log('name:', value);
+    });
+
+    /// control form readonly
     this.pageMode$.subscribe((value) => console.log('Page Mode:', value));
+    this.isFormReadOnly$.subscribe((isReadOnly) => {
+      console.log('isReadOnly:', isReadOnly);
+      this.basicForm.disable();
+    });
   }
 
   defaultMode() {
--- src/app/main-page-demo/main-page-demo.store.ts
+++ src/app/main-page-demo/main-page-demo.store.ts
@@ -1,23 +1,38 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, UntypedFormGroup } from '@angular/forms';
 import { ComponentStore } from '@ngrx/component-store';
 import { Observable } from 'rxjs';
 
 export interface MainPageDemoState {
   pageMode: string;
   isFormReadOnly: boolean;
+  basicForm: UntypedFormGroup;
 }
 
 @Injectable()
 export class MainPageDemoStore extends ComponentStore<MainPageDemoState> {
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    // default state is declare here
     super({
       pageMode: 'default',
       isFormReadOnly: false,
+      basicForm: fb.group({
+        name: [''],
+        age: 0,
+      }),
     });
   }
 
   readonly pageMode$: Observable<string> = this.select(
     (state) => state.pageMode
+  );
+
+  readonly isFormReadOnly$: Observable<boolean> = this.select(
+    (state) => state.isFormReadOnly
+  );
+
+  readonly basicForm$: Observable<UntypedFormGroup> = this.select(
+    (state) => state.basicForm
   );
 
   readonly setDefaultPageMode = this.updater((state) => ({
@@ -31,4 +46,10 @@ export class MainPageDemoStore extends ComponentStore<MainPageDemoState> {
     pageMode: 'add',
     isFormReadOnly: false,
   }));
+
+  // readonly setFormReadOnlyStatus = this.effect(
+  //   (pageMode: Observable<string>) => {
+  //     return;
+  //   }
+  // );
 }
